b3f3623dd7694364e35c37de789a0c988bfa5772
Ali A. AL-Darraji <sagads80@gmail.com>
2021-06-28 16:55:00 +0300

    added manual release for audioPath + sound in audio.m

--- src/audio.m
+++ src/audio.m
@@ -6,11 +6,14 @@ void PlayAlarm(char* gAudioPath);
 
 void PlayAlarm(char* gAudioPath)
 {
-	@autoreleasepool
-	{
+
     	NSString *audioPath = [[NSString alloc]initWithCString:gAudioPath encoding:NSASCIIStringEncoding];
+
 		NSSound *sound = [[NSSound alloc] initWithContentsOfFile:audioPath  byReference:NO];
 		[sound play];
-	}
-	
+
+		[audioPath release];
+		[sound release];
+
+
 }
