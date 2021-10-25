b5bcb5d0d4d6fcfc3fde51792febe699f5745734
Ali A <53301143+Haiderahandali@users.noreply.github.com>
2021-10-25 23:00:38 +0300

    Update draw_circle.c
    added comments for drawing the circle equation
    replaced multiplication with left-shift

--- src/draw_circle.c
+++ src/draw_circle.c
@@ -18,14 +18,14 @@ void DrawPixel(int x, int y)
 //https://www.bu.edu.eg/portal/uploads/Computers%20and%20Informatics/Computer%20Science/4899/crs-12417/Files/Midpoint%20Circle%20Algorithm.pdf
 void DrawCircle(int Cx, int Cy, int Radius)
 {
-    int X = -Radius;
+    int X = Radius;
     int Y = 0;
 
     int EP = 0;
     int EP0;
     int EP1;
 
-    while (-X >= Y)
+    while (X > Y)
     {
         DrawPixel(Cy + X, Cx + Y);
         DrawPixel(Cy + X, Cx - Y);
@@ -37,14 +37,18 @@ void DrawCircle(int Cx, int Cy, int Radius)
         DrawPixel(Cy - Y, Cx + X);
         DrawPixel(Cy - Y, Cx - X);
 
+        /* calculating the error for the next point and select the one with less error.
+         from the circle equation, the difference between the 2 errors is either 2X or 2Y
+         EP stands for the previous error, Ep1 is the error if we moved in the Y direction, Ep2 if we moved in the X direction
+        */
         EP += 1;
-        EP0 = EP + 2 * X;
-        EP1 = EP + 2 * Y;
+        EP0 = -EP + (X>>1);
+        EP1 = EP  + (Y>>1);
 
-        if (-EP0 <= EP1)
+        if (EP0 < EP1)
         {
-            EP = EP0;
-            ++X;
+            EP = -EP0;
+            --X;
         }
         else
         {
